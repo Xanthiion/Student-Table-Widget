972b9275c920c1c7ceafc7449d747aec23350471
Xanthiion <arjunkahlon100@gmail.com>
2020-10-01 13:31:17 -0700

    Fix: Removed Global Functions
    Sorted global functions into classes, mainly WidgetUI.
    WidgetUI object has to be instantiated for user to modify UI.
    Small improvements to function names and comments.

--- dist/app.js
+++ dist/app.js
@@ -24,7 +24,7 @@ class Storage {
   }
 
   // fill table with students in local storage
-  static displayStudents() {
+  static printStudents() {
     // retrieve students in local Storage
     const students = Storage.getStudents();
 
@@ -73,82 +73,146 @@ class Storage {
   }
 }
 
-// DOM Load Event
-document.addEventListener('DOMContentLoaded', Storage.displayStudents);
+// Widget Interface -- instantiate when modifying UI
+class WidgetUI {
+  addStudent(student) {
+    // Grab table element
+    const table = document.getElementById('student-table');
+
+    // Fill table entry with student information
+    const entry = document.createElement('tr');
+    entry.innerHTML = `
+      <td>${student.name}</td>
+      <td>${student.email}</td>
+      <td>${student.level}</td>
+      <td><a href="#" class="edit">Edit </a><a href="#" class="save">Save </a><a href="#" class="delete">X</a></td>
+    `;
+
+    // Print entry to table
+    table.appendChild(entry);
+  }
+
+  pingAlert(alert, alertType) {
+    const card = document.querySelector('.card');
+    const form = document.querySelector('#student-form');
+
+    // Create new alert div with given parameters
+    const div = document.createElement('div');
+    div.className = `alert ${alertType}`;
+    div.appendChild(document.createTextNode(alert));
+
+    // Push alert div to top of form
+    card.insertBefore(div, form);
+
+    // Remove Alert after 5 seconds
+    setTimeout(function () {
+      document.querySelector('.alert').remove();
+    }, 5000);
+  }
+
+  clearInputFields() {
+    // Set all input fields back to default
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('levels').value = 'default';
+  }
+
+  editStudent(student) {
+    // grab all elements within the entry
+    const name = student.querySelector('td:nth-child(1)'),
+      email = student.querySelector('td:nth-child(2)'),
+      level = student.querySelector('td:nth-child(3)'),
+      edit = student.querySelector('td:nth-child(4)');
+
+    // Copy values from elements
+    const nameVal = name.textContent,
+      emailVal = email.textContent,
+      levelVal = level.textContent;
+
+    // Change elements into input streams, set initial values to original content
+    name.innerHTML = `<input type="text" value="${nameVal}">`;
+    email.innerHTML = `<input type="email" value="${emailVal}">`;
+    level.innerHTML = `
+      <select name="levels" id="newLevels">
+        <option hidden disabled selected value='${levelVal}'>${levelVal}</option>
+        <option value="Freshman"> Freshman</option>
+        <option value="Sophomore">Sophomore</option>
+        <option value="Junior">Junior</option>
+        <option value="Senior">Senior</option>
+      </select>`;
+
+    // Make Edit button invisible and Save button visible
+    edit.firstChild.style.display = 'none';
+    edit.querySelector('a:nth-child(2)').style.display = 'inline';
+
+    // Remove previous entry from local Storage
+    Storage.removeStudent(emailVal);
+  }
+
+  saveStudent(student) {
+    // grab all elements within the entry
+    const name = student.querySelector('td:nth-child(1)'),
+      email = student.querySelector('td:nth-child(2)'),
+      level = document.getElementById('newLevels'),
+      save = student.querySelector('td:nth-child(4)');
+
+    // Copy values from elements
+    const nameVal = name.firstChild.value,
+      emailVal = email.firstChild.value,
+      levelVal = level.value;
+
+    // Replace elements with text nodes of their content
+    name.innerHTML = nameVal;
+    email.innerHTML = emailVal;
+    level.parentElement.innerHTML = levelVal;
+
+    // Make edit button visible and save invisible
+    save.firstChild.style.display = 'inline';
+    save.querySelector('a:nth-child(2)').style.display = 'none';
+
+    // Push new entry to local Storage
+    Storage.pushStudent(new Student(nameVal, emailVal, levelVal));
+  }
+}
+
+// DOM Load Event - Fill table with students from local storage
+document.addEventListener('DOMContentLoaded', Storage.printStudents);
 
 // Student Submit Listener
 document.getElementById('student-form').addEventListener('submit', function (e) {
+  const widget = new WidgetUI();
   // Pull values from input streams
   const name = document.getElementById('name').value,
     email = document.getElementById('email').value,
     level = document.getElementById('levels').value;
-  console.log(Storage.checkDuplicate(email));
+
   // Validate input
   if (name === '' || email === '' || level === 'default') {
     // Invalid Reuslts - empty field
-    pingAlert('Please fill in all fields', 'error');
+    widget.pingAlert('Please fill in all fields', 'error');
 
   } else if (Storage.checkDuplicate(email)) {
     // Invalid Results - duplicate entry
-    pingAlert('Student with this email already exists', 'error');
+    widget.pingAlert('Student with this email already exists', 'error');
+
   } else {
-    // Valid Results
-    // Add student to table, clear text fields, ping user: entry was successful
+    // Valid Results -- Add student, clear text fields, ping user
     const student = new Student(name, email, level);
-    addStudent(student);
+    widget.addStudent(student);
+    widget.clearInputFields();
+    widget.pingAlert('Student Added', 'success');
+
+    // Push student info to local storage
     Storage.pushStudent(student);
-    clearInputFields();
-    pingAlert('Student Added', 'success');
   }
+
   // Stop page refresh
   e.preventDefault();
 });
 
-
-function addStudent(student) {
-  // Grab table element
-  const table = document.getElementById('student-table');
-
-  // Fill table entry with student information
-  const entry = document.createElement('tr');
-  entry.innerHTML = `
-    <td>${student.name}</td>
-    <td>${student.email}</td>
-    <td>${student.level}</td>
-    <td><a href="#" class="edit">Edit </a><a href="#" class="save">Save </a><a href="#" class="delete">X</a></td>
-  `;
-
-  // Print entry to table
-  table.appendChild(entry);
-}
-
-function pingAlert(alert, alertType) {
-  const card = document.querySelector('.card');
-  const form = document.querySelector('#student-form');
-
-  // Create new alert div with given parameters
-  const div = document.createElement('div');
-  div.className = `alert ${alertType}`;
-  div.appendChild(document.createTextNode(alert));
-
-  // Push alert div to top of form
-  card.insertBefore(div, form);
-
-  // Remove Alert after 5 seconds
-  setTimeout(function () {
-    document.querySelector('.alert').remove();
-  }, 5000);
-}
-
-function clearInputFields() {
-  // Set all input fields back to default
-  document.getElementById('name').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('levels').value = 'default';
-}
-
 // Delete Entry Listener
 document.getElementById('student-table').addEventListener('click', function (e) {
+  const widget = new WidgetUI();
   // Check if click occured on Delete button
   if (e.target.className === 'delete') {
     // Remove tr element containing student
@@ -156,84 +220,32 @@ document.getElementById('student-table').addEventListener('click', function (e) 
     // Remove student from Local Storage
     Storage.removeStudent(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
     // Ping user that student was removed
-    pingAlert('Student Removed', 'success');
+    widget.pingAlert('Student Removed', 'success');
   }
   e.preventDefault();
 });
 
 // Edit Entry Listener
 document.getElementById('student-table').addEventListener('click', function (e) {
+  const widget = new WidgetUI();
   // Check if click was on edit button
   if (e.target.className === 'edit') {
     // change text nodes to text fields
-    makeModifiable(e.target.parentElement.parentElement);
+    widget.editStudent(e.target.parentElement.parentElement);
   }
   e.preventDefault();
 });
 
-function makeModifiable(element) {
-  // grab all elements within the entry
-  const name = element.querySelector('td:nth-child(1)'),
-    email = element.querySelector('td:nth-child(2)'),
-    level = element.querySelector('td:nth-child(3)'),
-    edit = element.querySelector('td:nth-child(4)');
 
-  // Copy values from elements
-  const nameVal = name.textContent,
-    emailVal = email.textContent,
-    levelVal = level.textContent;
-
-  // Change elements into input streams, set initial values to original content
-  name.innerHTML = `<input type="text" value="${nameVal}">`;
-  email.innerHTML = `<input type="email" value="${emailVal}">`;
-  level.innerHTML = `
-    <select name="levels" id="newLevels">
-      <option hidden disabled selected value='${levelVal}'>${levelVal}</option>
-      <option value="Freshman"> Freshman</option>
-      <option value="Sophomore">Sophomore</option>
-      <option value="Junior">Junior</option>
-      <option value="Senior">Senior</option>
-    </select>`;
-
-  // Make Edit button invisible and Save button visible
-  edit.firstChild.style.display = 'none';
-  edit.querySelector('a:nth-child(2)').style.display = 'inline';
-
-  // Remove previous entry from local Storage
-  Storage.removeStudent(emailVal);
-}
 
 // Save Entry Listener
 document.getElementById('student-table').addEventListener('click', function (e) {
+  const widget = new WidgetUI();
   // Check if click was on save button
   if (e.target.className === 'save') {
     // Set input streams back to text Nodes
-    makeConcrete(e.target.parentElement.parentElement);
+    widget.saveStudent(e.target.parentElement.parentElement);
   }
   e.preventDefault();
 });
 
-function makeConcrete(element) {
-  // grab all elements within the entry
-  const name = element.querySelector('td:nth-child(1)'),
-    email = element.querySelector('td:nth-child(2)'),
-    level = document.getElementById('newLevels'),
-    save = element.querySelector('td:nth-child(4)');
-
-  // Copy values from elements
-  const nameVal = name.firstChild.value,
-    emailVal = email.firstChild.value,
-    levelVal = level.value;
-
-  // Replace elements with text nodes of their content
-  name.innerHTML = nameVal;
-  email.innerHTML = emailVal;
-  level.parentElement.innerHTML = levelVal;
-
-  // Make edit button visible and save invisible
-  save.firstChild.style.display = 'inline';
-  save.querySelector('a:nth-child(2)').style.display = 'none';
-
-  // Push new entry to local Storage
-  Storage.pushStudent(new Student(nameVal, emailVal, levelVal));
-}
